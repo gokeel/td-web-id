d0a598c811bd939597822369688b38fc8b9b095d
Ocky Harliansyah <ocky.harli@gmail.com>
2017-09-13 10:48:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tutordoors2.0
---
+++ application/config/mail_smtp.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['smtp_host'] = 'srv18.niagahoster.com'; //change this
+$config['smtp_port'] = 465;
+$config['smtp_user'] = 'admin@tutordoors.com'; //change this
+$config['smtp_pass'] = 'm4k4ns14nggr4t1s'; //change this
